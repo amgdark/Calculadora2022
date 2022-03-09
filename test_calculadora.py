dcd09a774fdadec3f9d71b89c41017ecc427a2ae
Alejandro Mauricio González <amgdark@uaz.edu.mx>
2022-03-09 13:30:59 -0600

    Agrega funcionalidad de la operación resta, sus pruebas y configuración para TravisCI

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,15 +1,21 @@
 import unittest
 from calculadora import Calculadora
 
+
 class TestCalculadora(unittest.TestCase):
-    
+
     def test_2_mas_2(self):
         calc = Calculadora()
         self.assertEqual(4, calc.sumar(2, 2))
-        
-    def test_3_por_2(self):
+
+    def test_3_mas_2(self):
         calc = Calculadora()
         self.assertEqual(5, calc.sumar(3, 2))
         
+    def test_3_menos_2(self):
+        calc = Calculadora()
+        self.assertEqual(1, calc.restar(3, 2))
+
+
 if __name__ == '__main__':
     unittest.main()
