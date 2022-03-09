e2ce9b13f2ea56051d1318ced5bed84d168760e9
Alejandro Mauricio González <amgdark@uaz.edu.mx>
2022-03-09 13:35:18 -0600

    Corrige estándar de código

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,6 @@
 class Calculadora():
     def sumar(self, num1, num2):
         return num1 + num2
-    
+
     def restar(self, num1, num2):
         return num1 - num2
-    
--- test_calculadora.py
+++ test_calculadora.py
@@ -11,7 +11,7 @@ class TestCalculadora(unittest.TestCase):
     def test_3_mas_2(self):
         calc = Calculadora()
         self.assertEqual(5, calc.sumar(3, 2))
-        
+
     def test_3_menos_2(self):
         calc = Calculadora()
         self.assertEqual(1, calc.restar(3, 2))
